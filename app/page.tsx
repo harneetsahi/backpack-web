b5ac43282e749719a2d66ce9ba67d75ff522c9fc
Harneet <harneetsahi8@gmail.com>
2025-06-25 17:10:58 -0500

    add: home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 
 import Card from "./components/Card";
 import DetailedCard from "./components/DetailedCard";
+import Footer from "./components/Footer";
+import ImageCarousel from "./components/ImageCarousel";
 import MobileView from "./components/MobileView";
 import Navbar from "./components/Navbar";
 import { newCoins, popularCoins, topCoins } from "./data/cardData";
@@ -12,24 +14,28 @@ export default function Home() {
       <MobileView />
       <div className="hidden sm:block">
         <Navbar />
-      </div>
 
-      <div className=" flex justify-center items-center mx-5">
-        <div className=" flex flex-col gap-5 max-w-306  w-full ">
-          <div className="h-90 rounded-lg  border-1">image slider</div>
+        <div className=" flex justify-center items-center mx-5">
+          <div className=" flex flex-col gap-5 max-w-306  w-full ">
+            <div className=" w-full ">
+              <ImageCarousel />
+            </div>
 
-          <div className="mt-5 flex gap-4 justify-between w-full">
-            <Card title="New" children={newCoins} />
-            <Card title="Top Gainers" children={topCoins} />
-            <Card
-              className="hidden md:block"
-              title="Popular"
-              children={popularCoins}
-            />
+            <div className=" flex gap-4 justify-between w-full">
+              <Card title="New" children={newCoins} />
+              <Card title="Top Gainers" children={topCoins} />
+              <Card
+                className="hidden md:block"
+                title="Popular"
+                children={popularCoins}
+              />
+            </div>
+
+            <DetailedCard />
           </div>
-
-          <DetailedCard />
         </div>
+
+        <Footer />
       </div>
     </>
   );
