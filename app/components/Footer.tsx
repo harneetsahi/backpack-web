060b0cfca1e0e6fef02cc17fad09b6c6d91685a7
Harneet <harneetsahi8@gmail.com>
2025-06-25 17:08:32 -0500

    add: footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,134 @@
+import Link from "next/link";
+import { Logo } from "../icons/Logo";
+import { LogoText } from "../icons/LogoText";
+
+function Footer() {
+  return (
+    <>
+      <footer className="flex justify-center w-screen bg-[#14151B] mt-8 ">
+        <div className=" py-8 flex justify-between max-w-306 w-full px-5 ">
+          <div className=" flex flex-col justify-between">
+            <div className="flex items-center gap-3 ">
+              <Logo />
+              <LogoText />
+            </div>
+            <div className="text-[12px] flex md:gap-3 gap-2.5">
+              <p className="font-semibold w-max">Backpack Exchange © 2025</p>
+              <Link
+                href="https://support.backpack.exchange/legal"
+                className="text-gray-400 hover:text-gray-400/70"
+              >
+                Legal
+              </Link>
+              <Link
+                href="https://support.backpack.exchange/legal/general-legal/privacy-policy"
+                className="text-gray-400 hover:text-gray-400/70"
+              >
+                Privacy
+              </Link>
+            </div>
+          </div>
+
+          <div className="flex md:gap-10 gap-5 justify-between">
+            <div>
+              <p className="text-[13px] font-semibold pb-1.5">Company</p>
+              <div className="text-gray-400 text-[13px] flex flex-col gap-1.5">
+                <Link
+                  href="https://learn.backpack.exchange/about"
+                  className="text-gray-400 hover:text-gray-400/70"
+                >
+                  About
+                </Link>
+                <Link
+                  href="https://sugared-duckling-ec8.notion.site/7c1995182f4a491ba8a9501ee15cbd01?v=0604ca8976fa4e5e81c5494f7c95abc4"
+                  className="text-gray-400 hover:text-gray-400/70"
+                >
+                  Careers
+                </Link>
+                <Link
+                  href="https://support.backpack.exchange/"
+                  className="text-gray-400 hover:text-gray-400/70"
+                >
+                  Contact
+                </Link>
+              </div>
+            </div>
+
+            <div className="md:pl-15 pl-4  ">
+              <p className="text-[13px] font-semibold pb-1.5">Help & Support</p>
+              <div className="text-gray-400 text-[13px] flex flex-col gap-1.5">
+                <Link
+                  href="https://learn.backpack.exchange/"
+                  className="text-gray-400 hover:text-gray-400/70"
+                >
+                  Learn
+                </Link>
+                <Link
+                  href="https://guide.backpack.exchange/"
+                  className="text-gray-400 hover:text-gray-400/70"
+                >
+                  Guide
+                </Link>
+                <Link
+                  href="https://support.backpack.exchange/"
+                  className="text-gray-400 hover:text-gray-400/70"
+                >
+                  Support
+                </Link>
+                <Link
+                  href="https://docs.backpack.exchange/"
+                  className="text-gray-400 hover:text-gray-400/70"
+                >
+                  Documentation
+                </Link>
+              </div>
+            </div>
+
+            <div className="flex md:gap-4 gap-3 items-start md:pl-10 pl-4 ">
+              <Link href="https://x.com/backpack">
+                <img
+                  alt="x.com"
+                  loading="lazy"
+                  width="18"
+                  height="18"
+                  src="https://backpack.exchange/x-bw.svg"
+                ></img>
+              </Link>
+              <Link href="https://discord.com/invite/backpack">
+                <img
+                  alt="discord"
+                  loading="lazy"
+                  width="23"
+                  height="23"
+                  src="https://backpack.exchange/discord-bw.svg"
+                ></img>
+              </Link>
+
+              <Link href="https://www.linkedin.com/company/backpackexchange/">
+                <img
+                  alt="linkedin"
+                  loading="lazy"
+                  width="18"
+                  height="18"
+                  src="https://backpack.exchange/linkedin2.svg"
+                ></img>
+              </Link>
+
+              <Link href="https://www.reddit.com/r/Backpack_official/">
+                <img
+                  alt="reddit"
+                  loading="lazy"
+                  width="18"
+                  height="18"
+                  src="https://backpack.exchange/reddit2.svg"
+                ></img>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
