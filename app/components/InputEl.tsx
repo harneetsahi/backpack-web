2cc088ab2c9dd5dd5e0ece2839de81e5c54388f2
Harneet <harneetsahi8@gmail.com>
2025-06-26 20:02:34 -0500

    add: general input element

--- app/components/InputEl.tsx
+++ app/components/InputEl.tsx
@@ -5,18 +5,27 @@ interface InputElProps {
   name?: string;
   className?: string;
   icon?: React.ReactNode;
+  label?: string;
+  labelClass?: string;
+  defaultValue?: string | number;
 }
 
 function InputEl(props: InputElProps) {
   return (
-    <div>
-      <label htmlFor={props.id}></label>
+    <div className="flex flex-col">
+      <label
+        htmlFor={props.id}
+        className={`text-xs text-gray ${props.labelClass}`}
+      >
+        {props.label}
+      </label>
       <input
         type={props.type || "text"}
         placeholder={props.placeholder}
         id={props.id}
         name={props.name}
         className={`${props.className} `}
+        defaultValue={props.defaultValue}
       />
     </div>
   );
